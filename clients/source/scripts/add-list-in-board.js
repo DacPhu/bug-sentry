698af345e9e64f2b2f087508cc39710ac0f4a56b
DacPhu <87571273+DacPhu@users.noreply.github.com>
2024-04-25 09:52:52 +0700

    fix bug add board / task

--- clients/source/scripts/add-list-in-board.js
+++ clients/source/scripts/add-list-in-board.js
@@ -7,7 +7,6 @@ function addColumn() {
   const lastColumn = columns[columns.length - 1];
   const newColumn = document.createElement("div");
   newColumn.className = "column col-md-3";
-  newColumn.id = "column" + columnCount;
   newColumn.innerHTML = `
     <h3>Column ${columnCount}</h3>
     <ul id="column${columnCount}">
@@ -20,7 +19,6 @@ function addColumn() {
 
 function addTaskBox(columnId) {
   const column = document.getElementById(columnId);
-  const textBox = document.createElement("li");
-  textBox.innerHTML = '<input type="text" class="text-box">';
-  column.insertBefore(textBox, column.lastElementChild);
+  // const textBox = document.createElement("li");
+  column.innerHTML += '<li> <input type="text" class="text-box"> </li>';
 }
