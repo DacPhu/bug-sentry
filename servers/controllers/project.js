e336ff391fd442a97fa401690447c4ba45075f3c
DacPhu <87571273+DacPhu@users.noreply.github.com>
2024-06-02 14:17:42 +0700

    bug

--- servers/controllers/project.js
+++ servers/controllers/project.js
@@ -10,4 +10,24 @@ controller.showOverview = (req, res) => {
   res.render("project_overview");
 };
 
+controller.createProject = async (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.render("project", {
+      errorMessage: "Please fill all field",
+    });
+  }
+
+  try {
+    res.redirect("project");
+  } catch (error) {
+    console.error("Error creating project: ", error);
+    res.send("Can not create project!");
+  }
+};
+
+controller.addUser = async (req, res) => {};
+
+controller.removeUser = async (req, res) => {};
+controller.editUser = async (req, res) => {};
 module.exports = controller;
