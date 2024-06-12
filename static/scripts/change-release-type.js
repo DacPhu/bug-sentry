61bf9a02098dab9c2b3d18efe8636eee5a6f2fd0
dacphu <pldppldp123@gmail.com>
2024-06-12 09:59:17 +0700

    format

--- static/scripts/change-release-type.js
+++
@@ -1,7 +0,0 @@
-function showSection(sectionId) {
-  var sections = document.querySelectorAll("section.container");
-  for (var i = 0; i < sections.length; i++) {
-    sections[i].style.display = "none";
-  }
-  document.getElementById(sectionId).style.display = "block";
-}
