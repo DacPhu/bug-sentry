56cc97cc678941f5d41e7ddef55a887945b69437
Phan Le Dac Phu <phanledacphu@Phans-MacBook-Pro.local>
2024-06-20 14:29:47 +0700

    add file from client to server

--- routes/api/v1/attachment.js
+++ routes/api/v1/attachment.js
@@ -6,5 +6,6 @@ const controller = require("../../../controllers/api/v1/attachment");
 
 router.get("/", controller.getAttachments);
 router.get("/all", controller.getAllAttachments);
+router.post("/upload", controller.uploadFile);
 
 module.exports = router;
