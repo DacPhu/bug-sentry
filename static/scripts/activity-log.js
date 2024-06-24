7ccf8ee433bc1ab32a1d0f2ffa3b7919892cedae
AnGaRu <angaru@192.168.0.186>
2024-06-24 18:15:45 +0700

    fix activity_log view

--- static/scripts/activity-log.js
+++ static/scripts/activity-log.js
@@ -1,17 +1,17 @@
-// Function để hiển thị form add issue
-function showAddIssue() {
-  document.querySelector(".overlay").style.display = "block"; // bật overlay
-  document.querySelector(".add-issue-table").style.display = "block"; // bật add issue table
-}
+// // Function để hiển thị form add issue
+// function showAddIssue() {
+//   document.querySelector(".overlay").style.display = "block"; // bật overlay
+//   document.querySelector(".add-issue-table").style.display = "block"; // bật add issue table
+// }
 
-// Function để save issue
-function saveIssue() {
-  document.querySelector(".overlay").style.display = "none"; // tắt overlay
-  document.querySelector(".add-issue-table").style.display = "none"; // tắt add issue table
-}
+// // Function để save issue
+// function saveIssue() {
+//   document.querySelector(".overlay").style.display = "none"; // tắt overlay
+//   document.querySelector(".add-issue-table").style.display = "none"; // tắt add issue table
+// }
 
-// Function để cancel add issue
-function cancelAddIssue() {
-  document.querySelector(".overlay").style.display = "none"; // tắt overlay
-  document.querySelector(".add-issue-table").style.display = "none"; // tắt add issue table
-}
+// // Function để cancel add issue
+// function cancelAddIssue() {
+//   document.querySelector(".overlay").style.display = "none"; // tắt overlay
+//   document.querySelector(".add-issue-table").style.display = "none"; // tắt add issue table
+// }
