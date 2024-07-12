3a05beaf08bc00914a865197bb1e0fa07648f47c
AnGaRu2k3 <nguyenan19072003@gmail.com>
2024-07-12 19:44:40 +0700

    fix bug noti and add avatar

--- static/scripts/management/notification.js
+++ static/scripts/management/notification.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     const notificationCountElem = $("#notificationCount");
     const notificationListElem = $("#notificationList");
@@ -21,20 +20,24 @@ $(document).ready(function () {
                    
                     $.each(data, function (index, notification) {
                         const fullNameSender = notification.User.first_name + ' ' + notification.User.last_name
+                        console.log(notification)
                         
-                        
-                        const notificationItemHTML = `
-                        <a href="/project/${notification.project_id}/overview" class="text-decoration-none text-dark">
-                        <div class="container-notification-item d-flex my-2 " >
-                            <img src="${notification.User.profile_picture}" alt="Image Description"
-                            class="rounded-circle ms-3"
-                            style="width: 60px; height: 60px; object-fit: cover;" />
-    
-                            <h class="ms-3 flex-grow-1"><strong class="text-danger">${notification.Project.name} : ${fullNameSender}</strong> posted ${notification.content}</h>
-                        </div>
-                        </a>
-                        `
-                        notificationListElem.append(notificationItemHTML);
+                        if (notification.project_id && notification.User.profile_picture) {
+                            const notificationItemHTML = `
+                                <a href="/project/${notification.project_id}/overview" class="text-decoration-none text-dark">
+                                    <div class="container-notification-item d-flex my-2">
+                                        <img src="${notification.User.profile_picture}" alt="Image Description"
+                                            class="rounded-circle ms-3"
+                                            style="width: 60px; height: 60px; object-fit: cover;" />
+
+                                        <h class="ms-3 flex-grow-1"><strong class="text-danger">${notification.Project.name} : ${fullNameSender}</strong> posted ${notification.content}</h>
+                                    </div>
+                                </a>
+                            `;
+                            notificationListElem.append(notificationItemHTML);
+                        } else {
+                            console.error('Invalid notification data:', notification);
+                        }
                     });
                 } else {
                     notificationCountElem.text("0");
