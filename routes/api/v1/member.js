7baa65125e7a2644171cfa097d1698c9bd34e8db
Phan Le Dac Phu <phanledacphu@Phans-MacBook-Pro.local>
2024-07-13 19:01:51 +0700

    add user

--- routes/api/v1/member.js
+++ routes/api/v1/member.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const controller = require("../../../controllers/api/v1/member");
 
 router.get("/:id", controller.getMembersInProject);
-router.post("/:id", controller.addUserToProject);
+router.post("/add-user", controller.addUserToProject);
 
 module.exports = router;
