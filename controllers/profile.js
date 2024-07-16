57073bbe32625fc6a19eddb2d50eae1cdabb744a
DacPhu <87571273+DacPhu@users.noreply.github.com>
2024-07-16 21:00:31 +0700

    fix bug role name

--- controllers/profile.js
+++ controllers/profile.js
@@ -5,11 +5,18 @@ const models = require("../models");
 
 controller.showEditProfileForm = async (req, res) => {
   const userId = req.session.userId;
+  console.log(req);
 
   try {
     const user = await models.User.findByPk(userId);
-    console.log(user);
-    res.render("editProfile", { user });
+    console.log("____________USER", user);
+    let role_name = "Project manager";
+    if (user.role_id == 2) {
+      role_name = "Tester";
+    } else if (user.role_id == 1) {
+      role_name = "Developer";
+    }
+    res.render("editProfile", { user, role_name });
   } catch (error) {
     console.error(error);
   }
